690c67912dc984cb02464e247acd123625c1f7ed
kc2732 <kc2732@nyu.edu>
2016-06-13 04:21:24 -0400

    utf8

--- parts/head.php
+++ parts/head.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset='utf-8' />
-    <meta http-equiv="Content-Type"content="text/html; charset=gb2312">
+    <meta http-equiv="Content-Type"content="text/html; charset=utf-8">
     <meta name="viewport" content="width=device-width" />
     <link rel="shortcut icon" href="../favicon.ico">
     <link rel='stylesheet' href='../lib/cupertino/jquery-ui.min.css' />
