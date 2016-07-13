6e34c5573ee22c73bc6b4852b39ce1e6517e92db
unknown <kc2732@nyu.edu>
2016-07-13 14:15:25 -0400

    okay

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: PC
+ * Date: 2016/7/13
+ * Time: 14:07
+ */
+header('Location:pages');
+?>
--- parts/top-bar.php
+++ parts/top-bar.php
@@ -1,15 +1,13 @@
-
 <div class="top-bar">
     <ul>
         <li>
-
             <a href="../pages/index.php" <?php if($phpself == 'index.php') echo "style='font-weight:600'"; ?>>首页</a>
         </li>
         <li>
             <a href="../pages/plan.php" <?php if($phpself == 'plan.php') echo "style='font-weight:600'"; ?>>运动处方</a>
         </li>
         <li>
-            <a href="../pages/data.php" <?php if($phpself == 'Data.php') echo "style='font-weight:600'"; ?>>数据监测</a>
+            <a href="../pages/data-collection.php" <?php if($phpself == 'data-collection.php') echo "style='font-weight:600'"; ?>>数据监测</a>
         </li>
         <li>
             <a href="../pages/lecture.php" <?php if($phpself == 'lecture.php') echo "style='font-weight:600'"; ?>>健康讲堂</a>
@@ -21,5 +19,4 @@
             <a class="button-submit" href="../pages/signup.php">注册/登录</a>
         </li>
     </ul>
-
 </div>
