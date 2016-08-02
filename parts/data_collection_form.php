0eaba04a5378592168e5e43ce8a26e3565ed51a2
unknown <kc2732@nyu.edu>
2016-08-02 19:23:46 -0400

    Changes on YundongChufang and Form

--- parts/data_collection_form.php
+++ parts/data_collection_form.php
@@ -226,8 +226,8 @@ if($_SESSION['id']!=null){
                 <h4>你曾经是否有过胸部疼痛？*</h4>
                 <input type="radio" name="question50" id="question501" value="1" <?php if($row['question50']==1) echo "ng-checked='true'";?> ng-required="part2==1">是
                 <input type="radio" name="question50" id="question502" value="2" <?php if($row['question50']==2) echo "ng-checked='true'";?> ng-required="part2==1">否
-                <h4>你曾经是否有过呼吸短促*</h4>
-                <input type="radio" name="question51" id="question511" value="1" <?php if($row['question51']==1) echo "ng-checked='true'";?> ng-required="part2==1">是
+                <!--<h4>你曾经是否有过呼吸短促*</h4>
+                <input type="radio" name="question51" id="question511" value="1"--> <?php //if($row['question51']==1) echo "ng-checked='true'";?> <!--ng-required="part2==1">是-->
                 <input type="radio" name="question51" id="question512" value="2" <?php if($row['question51']==2) echo "ng-checked='true'";?> ng-required="part2==1">否
                 <h4>你曾经是否有过心悸？*</h4>
                 <input type="radio" name="question52" id="question521" value="1" <?php if($row['question52']==1) echo "ng-checked='true'";?> ng-required="part2==1">是
