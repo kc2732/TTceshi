b3a0b703720f6dc8fea73eb30508f79da9c3c18d
kc2732 <kc2732@nyu.edu>
2016-12-16 15:21:15 -0500

    content decla

--- pages/shengming.php
+++ pages/shengming.php
@@ -6,7 +6,7 @@ include '../parts/head.php';
 <span>请访问糖糖运动医生官方网站：</span><br>
 <span><a href="http://www.tangtangyundong.com">www.tangtangyundong.com</a></span><br>
 <span style="color:red;">声明：</span><br>
-<span>您现在访问的网站正在恶意解析糖糖运动医生的服务器，请各位用户认准糖糖运动医生唯一指定官方网站，谨防受骗上当</span>
+<span>近日发现糖糖运动医生网站正在遭受恶意解析，请各位用户认准糖糖运动医生唯一指定官方网站，谨防受骗上当</span>
 </div>
 
 <?php
