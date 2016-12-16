b7d7118fb2c157b392ed293703a18432bcdf26ea
kc2732 <kc2732@nyu.edu>
2016-12-16 14:59:09 -0500

    declaration

---
+++ pages/shengming.php
@@ -0,0 +1,14 @@
+<?php
+
+include '../parts/head.php';
+?>
+<div class="container" style="font-weight:600; font-size:36px">
+<span>请访问糖糖运动医生官方网站：</span><br>
+<span><a href="http://www.tangtangyundong.com">www.tangtangyundong.com</a></span><br>
+<span style="color:red;">声明：</span><br>
+<span>您现在访问的网站正在恶意解析糖糖运动医生的服务器，请各位用户认准糖糖运动医生唯一指定官方网站，谨防受骗上当</span>
+</div>
+
+<?php
+include '../parts/update.php';
+?>
